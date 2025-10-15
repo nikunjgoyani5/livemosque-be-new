2234e0f4c28adb9b0d6ce90a9d750bd088648be3
yashpatoliya <yashpatoliya333@gmail.com>
2025-10-16 00:19:59 +0530

    FIX | cors

--- src/index.ts
+++ src/index.ts
@@ -14,13 +14,12 @@ connectDB();
 const PORT = process.env.PORT || 5000;
 const app = express();
 // Allow CORS with JSON body
-// app.use(
-//   cors({
-//     origin: "*",
-//     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
-//     allowedHeaders: ["Content-Type", "Authorization"],
-//   })
-// );
+app.use(
+  cors({
+    origin: ["http://localhost:3000", "https://livemosque-beta.vercel.app"],
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
