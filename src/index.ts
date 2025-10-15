cd6544500237b225dd77af16206ac0259f9799ee
yashpatoliya <yashpatoliya333@gmail.com>
2025-10-15 23:50:37 +0530

    FIX | comment out CORS middleware configuration

--- src/index.ts
+++ src/index.ts
@@ -14,13 +14,13 @@ connectDB();
 const PORT = process.env.PORT || 5000;
 const app = express();
 // Allow CORS with JSON body
-app.use(
-  cors({
-    origin: "*",
-    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-  })
-);
+// app.use(
+//   cors({
+//     origin: "*",
+//     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+//     allowedHeaders: ["Content-Type", "Authorization"],
+//   })
+// );
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
